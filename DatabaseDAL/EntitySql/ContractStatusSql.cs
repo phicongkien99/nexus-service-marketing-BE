2343a7aa95eea08358713808dd6f6da024c2a5be
khoivusixone <khoi.vuvan@sixone.vn>
2020-11-09 22:59:17 +0700

    update code

---
+++ DatabaseDAL/EntitySql/ContractStatusSql.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Nexus.Entity;
+using Nexus.Entity.Entities;
+using Nexus.DatabaseDAL.Common;
+using System.Data;
+using Anotar.NLog;
+
+namespace Nexus.DatabaseDAL.EntitySql
+{
+	public class ContractStatusSql : EntityBaseSql 
+	{
+
+        #region Constructor
+
+		public ContractStatusSql()
+		{
+			 Init();
+		}
+
+        #endregion
+		
+		#region Init
+
+		private void Init()
+		{
+           Insertsql = "ContractStatus_Insert";
+           Updatesql = "ContractStatus_Update";
+           Deletesql = "ContractStatus_DeleteByPrimaryKey";
+           Selectsql = "ContractStatus_SelectAll";
+		}
+
+        #endregion
+
+        #region Public override Methods
+
+        public override SqlCommand GetSqlCommandForInsert(BaseEntity baseEntity)
+		{
+			var sqlCommand = new SqlCommand { CommandType = CommandTypeProcedure };
+            if (baseEntity != null)
+            {
+                var businessObject = baseEntity as ContractStatus;
+                if (businessObject != null)
+				{
+
+								sqlCommand.Parameters.Add(new SqlParameter("@CreatedAt", SqlDbType.Text, 8, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.CreatedAt));
+				sqlCommand.Parameters.Add(new SqlParameter("@CreatedBy", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.CreatedBy));
+				sqlCommand.Parameters.Add(new SqlParameter("@Description", SqlDbType.VarChar, 255, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Description));
+				sqlCommand.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Id));
+				sqlCommand.Parameters.Add(new SqlParameter("@IsDeleted", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.IsDeleted));
+				sqlCommand.Parameters.Add(new SqlParameter("@Name", SqlDbType.VarChar, 255, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Name));
+				sqlCommand.Parameters.Add(new SqlParameter("@UpdatedAt", SqlDbType.Text, 8, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.UpdatedAt));
+				sqlCommand.Parameters.Add(new SqlParameter("@UpdatedBy", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.UpdatedBy));
+
+
+				}
+			}
+            return sqlCommand;	
+		}
+
+		public override BaseEntity UpdateEntityId(BaseEntity baseEntity, SqlCommand sqlCommand)
+        {
+            if (baseEntity != null)
+            {
+                return baseEntity;
+                var businessObject = baseEntity as ContractStatus;
+
+                if (businessObject != null)
+                {
+                
+                }
+                return businessObject;
+            }
+            return null;
+        }
+
+        public override SqlCommand GetSqlCommandForUpdate(BaseEntity baseEntity)
+        {
+            var sqlCommand = new SqlCommand { CommandType = CommandTypeProcedure };
+            if (baseEntity != null)
+            {
+                var businessObject = baseEntity as ContractStatus;
+
+                if (businessObject != null)
+                {
+
+								sqlCommand.Parameters.Add(new SqlParameter("@CreatedAt", SqlDbType.Text, 8, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.CreatedAt));
+				sqlCommand.Parameters.Add(new SqlParameter("@CreatedBy", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.CreatedBy));
+				sqlCommand.Parameters.Add(new SqlParameter("@Description", SqlDbType.VarChar, 255, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Description));
+				sqlCommand.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Id));
+				sqlCommand.Parameters.Add(new SqlParameter("@IsDeleted", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.IsDeleted));
+				sqlCommand.Parameters.Add(new SqlParameter("@Name", SqlDbType.VarChar, 255, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Name));
+				sqlCommand.Parameters.Add(new SqlParameter("@UpdatedAt", SqlDbType.Text, 8, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.UpdatedAt));
+				sqlCommand.Parameters.Add(new SqlParameter("@UpdatedBy", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.UpdatedBy));
+
+				
+				}
+			}       
+         return sqlCommand;
+		}
+        
+        public override SqlCommand GetSqlCommandForDelete(BaseEntity baseEntity)
+		{
+            var sqlCommand = new SqlCommand {CommandType = CommandTypeProcedure};
+            if (baseEntity != null)
+            {
+                var businessObject = baseEntity as ContractStatus;
+                if (businessObject != null)
+                {
+
+								sqlCommand.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Proposed, businessObject.Id));
+
+				
+				}
+            }
+            return sqlCommand;
+        }
+        
+		public override List<BaseEntity> PopulateBusinessObjectFromReader(IDataReader dataReader)
+        {
+			var list = new List<BaseEntity>();
+            _dicIndex = new Dictionary<string, int>();
+            while (dataReader.Read())
+            {
+                var businessObject = new ContractStatus();
+                PopulateBusinessObjectFromReader(businessObject, dataReader);
+                list.Add(businessObject);
+            }
+
+            return list;
+		}     
+
+        #endregion
+
+        #region Private Methods
+        public static Dictionary<string, int> _dicIndex = new Dictionary<string, int>();
+	    public static void FillDicIndex(IDataReader dataReader)
+	    {
+	        for (int i = 0; i < dataReader.FieldCount; i++)
+	        {
+	            var columnName = dataReader.GetName(i);
+	            _dicIndex[columnName.ToLower()] = i;
+	        }
+	    }
+	    public static int GetIndex(string name)
+	    {
+	        if (_dicIndex.ContainsKey(name.ToLower()))
+	            return _dicIndex[name.ToLower()];
+	        //LogTo.Info("Khong tim thay column name = " + name.ToLower());
+	        return -1;
+	    }
+
+    internal void PopulateBusinessObjectFromReader(ContractStatus businessObject, IDataReader dataReader)
+        {
+            if (_dicIndex.Count == 0) FillDicIndex(dataReader);
+		
+			if (GetIndex(ContractStatus.ContractStatusFields.CreatedAt.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.CreatedAt.ToString())))
+				{
+					businessObject.CreatedAt = dataReader.GetDateTime(GetIndex(ContractStatus.ContractStatusFields.CreatedAt.ToString()));
+				}
+
+			if (GetIndex(ContractStatus.ContractStatusFields.CreatedBy.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.CreatedBy.ToString())))
+				{
+					businessObject.CreatedBy = dataReader.GetInt32(GetIndex(ContractStatus.ContractStatusFields.CreatedBy.ToString()));
+				}
+
+			if (GetIndex(ContractStatus.ContractStatusFields.Description.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.Description.ToString())))
+				{
+					businessObject.Description = dataReader.GetString(GetIndex(ContractStatus.ContractStatusFields.Description.ToString()));
+				}
+
+				businessObject.Id = dataReader.GetInt32(GetIndex(ContractStatus.ContractStatusFields.Id.ToString()));
+
+			if (GetIndex(ContractStatus.ContractStatusFields.IsDeleted.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.IsDeleted.ToString())))
+				{
+					businessObject.IsDeleted = dataReader.GetInt32(GetIndex(ContractStatus.ContractStatusFields.IsDeleted.ToString()));
+				}
+
+			if (GetIndex(ContractStatus.ContractStatusFields.Name.ToString()) != -1)
+				businessObject.Name = dataReader.GetString(GetIndex(ContractStatus.ContractStatusFields.Name.ToString()));
+
+			if (GetIndex(ContractStatus.ContractStatusFields.UpdatedAt.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.UpdatedAt.ToString())))
+				{
+					businessObject.UpdatedAt = dataReader.GetDateTime(GetIndex(ContractStatus.ContractStatusFields.UpdatedAt.ToString()));
+				}
+
+			if (GetIndex(ContractStatus.ContractStatusFields.UpdatedBy.ToString()) != -1)
+				if (!dataReader.IsDBNull(GetIndex(ContractStatus.ContractStatusFields.UpdatedBy.ToString())))
+				{
+					businessObject.UpdatedBy = dataReader.GetInt32(GetIndex(ContractStatus.ContractStatusFields.UpdatedBy.ToString()));
+				}
+
+
+        }
+
+        #endregion
+	}
+}
---
+++ Nexus/Controllers/ContractStatusController.cs
@@ -0,0 +1,295 @@
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Nexus.Common.Enum;
+using System.Web.Http.Cors;
+using Nexus.DatabaseDAL.Common;
+using Nexus.Entity;
+using Nexus.Entity.Entities;
+using Nexus.Memory;
+using Nexus.Models;
+using Nexus.Utils;
+namespace Nexus.Controllers
+{
+	public class ContractStatusController: ApiController
+	{
+		[EnableCors(origins: "*", headers: "*", methods: "*")]
+		public async Task<IHttpActionResult> Get()
+		{
+			try
+			{
+				#region token
+				var header = Request.Headers;
+				if (header.Authorization == null)
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				var token = header.Authorization.Parameter;
+				Employee employee;
+				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out employee))
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				#endregion
+				var lstData = MemoryInfo.GetAllContractStatus();
+				var res = new RequestErrorCode(true, null, null);
+				res.ListDataResult.AddRange(lstData);
+				return Ok(res);
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+			}
+			return BadRequest("Unknow");
+		}
+
+		[EnableCors(origins: "*", headers: "*", methods: "*")]
+		public async Task<IHttpActionResult> Get(int id)
+		{
+			try
+			{
+				#region token
+				var header = Request.Headers;
+				if (header.Authorization == null)
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				var token = header.Authorization.Parameter;
+				Employee employee;
+				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out employee))
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				#endregion
+				var data = MemoryInfo.GetContractStatus(id);
+				var res = new RequestErrorCode(true, null, null);
+				res.DataResult = data;
+				return Ok(res);
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+			}
+			return BadRequest("Unknow");
+		}
+
+		[EnableCors(origins: "*", headers: "*", methods: "*")]
+		public async Task<IHttpActionResult> Post([FromBody]ContractStatus req)
+		{
+			try
+			{
+				string errorMessage = "UnknowError";
+				string errorCode = ErrorCodeEnum.UnknownError.ToString();
+				#region token
+				var header = Request.Headers;
+				if (header.Authorization == null)
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				var token = header.Authorization.Parameter;
+				Employee employee;
+				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out employee))
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				#endregion
+
+				#region Validate
+				if (!Validate(req, out errorCode, out errorMessage))
+				{
+					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+				}
+				#endregion
+
+				#region Tạo key
+				var oldKey = Memory.Memory.GetMaxKey(req.GetName());
+				int newKey = oldKey + 1;
+				// set key
+				req.Id = newKey;
+				#endregion
+
+				#region Process
+				req.CreatedAt = DateTime.Now;
+				req.CreatedBy = employee.Id;
+				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+				var lstCommand = new List<EntityCommand>();
+				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(req), EntityAction = EntityAction.Insert });
+				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+				if (!isOkDone)
+				{
+					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+				}
+				#endregion
+				// update memory
+				MemorySet.UpdateAndInsertEntity(req);
+				var result = new RequestErrorCode(true);
+				result.DataResult = req;
+				return Ok(result);
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+			}
+			return BadRequest("Unknow");
+		}
+
+		[EnableCors(origins: "*", headers: "*", methods: "*")]
+		public async Task<IHttpActionResult> Put(int id,[FromBody]ContractStatus req)
+		{
+			try
+			{
+				string errorMessage = "UnknowError";
+				string errorCode = ErrorCodeEnum.UnknownError.ToString();
+				#region token
+				var header = Request.Headers;
+				if (header.Authorization == null)
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				var token = header.Authorization.Parameter;
+				Employee employee;
+				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out employee))
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				#endregion
+
+				#region Validate
+				if (!ValidateUpdate(req, out errorCode, out errorMessage))
+				{
+					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+				}
+				#endregion
+
+				#region Check exist
+				var obj = MemoryInfo.GetContractStatus(id);
+				if (obj == null)
+				{
+					return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
+				}
+				#endregion
+				req.Id = obj.Id; // gan lai id de update
+				#region Process
+				req.UpdatedAt = DateTime.Now;
+				req.UpdatedBy = employee.Id;
+				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+				var lstCommand = new List<EntityCommand>();
+				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(req), EntityAction = EntityAction.Update });
+				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+				if (!isOkDone)
+				{
+					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+				}
+				#endregion
+				// update memory
+				MemorySet.UpdateAndInsertEntity(req);
+				var result = new RequestErrorCode(true);
+				result.DataResult = req;
+				return Ok(result);
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+			}
+			return BadRequest("Unknow");
+		}
+
+		[EnableCors(origins: "*", headers: "*", methods: "*")]
+		public async Task<IHttpActionResult> Delete(int id)
+		{
+			try
+			{
+				string errorMessage = "UnknowError";
+				string errorCode = ErrorCodeEnum.UnknownError.ToString();
+				#region token
+				var header = Request.Headers;
+				if (header.Authorization == null)
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				var token = header.Authorization.Parameter;
+				Employee employee;
+				if (string.IsNullOrWhiteSpace(token) || !TokenManager.ValidateToken(token, out employee))
+				{
+					return StatusCode(HttpStatusCode.Unauthorized);
+				}
+				#endregion
+
+				#region Check exist
+				var obj = MemoryInfo.GetContractStatus(id);
+				if (obj == null)
+				{
+					return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
+				}
+				#endregion
+
+				bool isHasDeleteProperties = obj.GetType().GetProperty("IsDeleted") != null;
+				if (!isHasDeleteProperties)
+				{
+					return Ok(new RequestErrorCode(false, ErrorCodeEnum.DataNotExist.ToString(), "Khong ton tai"));
+				}
+				obj.IsDeleted = 1;
+
+				#region Process
+				UpdateEntitySql updateEntitySql = new UpdateEntitySql();
+				var lstCommand = new List<EntityCommand>();
+				lstCommand.Add(new EntityCommand { BaseEntity = new Entity.Entity(obj), EntityAction = EntityAction.Update });
+				bool isOkDone = updateEntitySql.UpdateDefault(lstCommand);
+				if (!isOkDone)
+				{
+					return Ok(new RequestErrorCode(false, errorCode, errorMessage));
+				}
+				#endregion
+				// update memory
+				MemorySet.UpdateAndInsertEntity(obj);
+				var result = new RequestErrorCode(true);
+				result.DataResult = obj;
+				return Ok(result);
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+			}
+			return BadRequest("Unknow");
+		}
+
+		#region Validation
+		private bool Validate(ContractStatus obj, out string errorCode, out string errorMess)
+		{
+			errorCode = null;
+			errorMess = null;
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+				throw;
+			}
+			return true;
+		}
+
+		private bool ValidateUpdate(ContractStatus obj, out string errorCode, out string errorMess)
+		{
+			errorCode = null;
+			errorMess = null;
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				Logger.Write(ex.ToString());
+				throw;
+			}
+			return true;
+		}
+		#endregion
+
+
+	}
+}
+
