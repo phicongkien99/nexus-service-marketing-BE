04f8bc94ab016e2ada1bd937b30c8685046b1ca4
khoivusixone <khoi.vuvan@sixone.vn>
2020-11-11 20:51:27 +0700

    update code

--- Nexus/Properties/AssemblyInfo.cs
+++ Nexus/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ElectricShop")]
+[assembly: AssemblyTitle("Nexus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElectricShop")]
+[assembly: AssemblyProduct("Nexus")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
